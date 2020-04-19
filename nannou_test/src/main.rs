d807b7a9e300061229a06de740661e63d375f8ec
piteredo <piteredo@hotmail.com>
2020-04-19 23:07:43 +0900

    change

--- nannou_test/src/main.rs
+++ nannou_test/src/main.rs
@@ -1,4 +1,5 @@
 use nannou::prelude::*;
+use rand::Rng;
 
 fn main() {
     nannou::app(model)
@@ -6,25 +7,52 @@ fn main() {
         .run();
 }
 
-struct Model {}
+struct Model {
+    x: f32,
+    y: f32,
+}
 
 fn model(app: &App) -> Model {
     let _window = app
-        .new_window() // newして
-        .size(1920, 1080)
+        .new_window()
+        .event(event)
         .view(view)
-        .build() // buildする
+        .build()
         .expect("err");
-    Model {}
+    Model {
+        x: 0.0,
+        y: 0.0,
+    }
 }
 
+fn event(_app: &App, model: &mut Model, event: WindowEvent) {
+    println!("{:?}", event);
+
+    // キーイベントによって update されるものはここで update
+    match event {
+        KeyPressed(_key) => {
+            model.x += 5.0;
+            model.y += 5.0;
+        }
+
+        _other => {}
+    }
+}
+
+// それ以外の条件によって update されるものはこっち
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 
 }
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background()
-        .color(OLIVE);
+        .color(DIMGRAY);
+
+    draw.ellipse()
+        .x_y(model.x, model.y)
+        .radius(10.0)
+        .color(RED);
+
     draw.to_frame(app, &frame).expect("err");
 }
