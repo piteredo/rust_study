33979a0cc7ff081c1cfe70763ef944b02f609393
piteredo <piteredo@hotmail.com>
2020-04-27 01:31:37 +0900

    updt

--- nannou_test/src/main.rs
+++ nannou_test/src/main.rs
@@ -1,48 +1,52 @@
 use nannou::prelude::*;
-//use rand::Rng;
-use nannou::text::*;
+use rand::Rng; // gen_range() に必要
 
 fn main() {
     nannou::app(model)
         .update(update)
+        .view(view)
         .run();
 }
 
-struct Model {
+struct Ball {
     x: f32,
     y: f32,
+    _goal_x: f32,
+    _goal_y: f32,
+    radius: f32,
 }
 
-fn model<'a>(app: &App) -> Model {
-    let _window = app
-        .new_window()
-        .event(event)
-        .view(view)
+struct Model {
+    balls: Vec<Ball>,
+}
+
+fn model(app: &App) -> Model {
+    let window_w = 800;
+    let window_h = 600;
+    app.set_loop_mode(LoopMode::rate_fps(12.0)); // 効いてない？
+    app.new_window()
+        .size(window_w, window_h)
         .build()
-        .expect("err");
-    //let font_data: &[u8] = include_bytes!("../Jazz____.ttf");
-    //let font: Font = Font::from_bytes(font_data).unwrap();
-    Model {
-        x: 0.0,
-        y: 0.0,
-    }
-}
+        .unwrap();
 
-fn event(_app: &App, model: &mut Model, event: WindowEvent) {
-    println!("{:?}", event);
-
-    // キーイベントによって update されるものはここで update
-    match event {
-        KeyPressed(_key) => {
-            model.x += 5.0;
-            model.y += 5.0;
+    let mut balls: Vec<Ball> = Vec::new();
+    let size = 30;
+    for x in 0..size {
+        for y in 0..size {
+            let ball = Ball {
+                x: rand::thread_rng().gen_range(-(window_w as f32)/2.0, window_w as f32/2.0),
+                y: rand::thread_rng().gen_range(-(window_h as f32)/2.0, window_h as f32/2.0),
+                radius: 10.0,
+                _goal_x: (x * 20) as f32 - (window_w as f32)/2.0,
+                _goal_y: (y * 20) as f32 - (window_h as f32)/2.0,
+            };
+            balls.push(ball);
         }
-
-        _other => {}
     }
+
+    Model { balls: balls }
 }
 
-// それ以外の条件によって update されるものはこっち
 fn update(_app: &App, _model: &mut Model, _update: Update) {
 
 }
@@ -50,18 +54,17 @@ fn update(_app: &App, _model: &mut Model, _update: Update) {
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background()
-        .color(DIMGRAY);
+        .color(SKYBLUE);
 
-    draw.ellipse()
-        .x_y(model.x, model.y)
-        .radius(10.0)
-        .color(RED);
+    for i in &model.balls {
+        draw.rect()
+            .x_y(i.x, i.y)
+            .w_h(i.radius, i.radius)
+            .color(BLUE);
+    }
 
+    draw.text(&app.fps().to_string())
+        .font_size(30);
 
-    let font_data: &[u8] = include_bytes!("../arial.ttf");
-    let font: Font = Font::from_bytes(font_data).unwrap();
-    let text = ",;p./:@][";
-    draw.text(text).font_size(60).font(font);
-
-    draw.to_frame(app, &frame).expect("err");
+    draw.to_frame(app, &frame).unwrap();
 }
