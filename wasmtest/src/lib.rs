ac69f1a2d04916a2c3dae3a9baa0332ced882cac
piteredo <piteredo@hotmail.com>
2020-08-04 08:13:42 +0900

    updt

--- wasmtest/src/lib.rs
+++ wasmtest/src/lib.rs
@@ -1,23 +1,100 @@
 extern crate wasm_bindgen;
 
+use std::f64;
+use std::cell::RefCell;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn body() -> web_sys::HtmlElement {
+    document().body().expect("document should have a body")
 }
 
 #[wasm_bindgen(start)]
 pub fn run() -> Result<(), JsValue> {
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
 
+    let canvas = document().get_element_by_id("canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .map_err(|_| ())
+        .unwrap();
 
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-    let val = document.create_element("Button")?;
-    val.set_attribute("onclick", "alert('dd')");
-    val.set_inner_html("Hello from Rust!");
-    body.append_child(&val)?;
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
 
+    let mut i = 0.0;
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        if i > 300.0 {
+            //body().set_text_content(Some("All done!"));
+
+            // Drop our handle to this closure so that it will get cleaned
+            // up once we return.
+            let _ = f.borrow_mut().take();
+            return;
+        }
+
+        i += 1.0;
+
+        context.set_fill_style(&"#AAAAAA".into());
+        context.set_line_width(3.0);
+        context.fill_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
+        context.begin_path();
+
+        // Draw the outer circle.
+        context
+            .arc(75.0 + i, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        // Draw the mouth.
+        context.move_to(110.0, 75.0);
+        context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+        // Draw the left eye.
+        context.move_to(65.0, 65.0);
+        context
+            .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        // Draw the right eye.
+        context.move_to(95.0, 65.0);
+        context
+            .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+            .unwrap();
+
+        context.stroke();
+
+        // Set the body's text content to how many times this
+        // requestAnimationFrame callback has fired.
+        //i += 1;
+        //let text = format!("requestAnimationFrame has been called {} times.", i);
+        //body().set_text_content(Some(&text));
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(f.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
     Ok(())
 }
