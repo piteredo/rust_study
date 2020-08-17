fa35cd5df44d295e6c5fa9e73e51413a87110301
piteredo <piteredo@hotmail.com>
2020-08-17 23:31:58 +0900

    updt

--- nannou_test/src/main.rs
+++ nannou_test/src/main.rs
@@ -4,7 +4,7 @@ use rand::Rng; // gen_range() に必要
 
 fn main() {
     nannou::app(model)
-        .update(update)
+        //.update(update)
         .view(view)
         .run();
 }
@@ -14,26 +14,28 @@ struct Model(u8, u8, u8);
 fn model(app: &App) -> Model {
     let window_w = 800;
     let window_h = 600;
+
+    app.set_loop_mode(LoopMode::rate_fps(24.0));
     app.new_window()
         .size(window_w, window_h)
         .build()
         .unwrap();
 
+
     Model (50, 50, 50)
 }
 
-fn update(app: &App, model: &mut Model, _update: Update) {
-    app.set_loop_mode(LoopMode::rate_fps(1.0));
-    model.0 += 1;
+fn update(_app: &App, model: &mut Model, _update: Update) {
+    /*model.0 += 1;
     model.1 += 1;
-    model.2 += 1;
+    model.2 += 1;*/
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
-
     let draw = app.draw();
     draw.background()
         .color(Rgb::new(model.0, model.1, model.2));
 
+    draw.text(&format!("{}", app.elapsed_frames()));
     draw.to_frame(app, &frame).unwrap();
 }
